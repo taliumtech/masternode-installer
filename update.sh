038cf841e63bee14ed2408eda44f00b8507108f2
taliumtech <37536531+taliumtech@users.noreply.github.com>
2019-02-02 17:24:06 +0200

    irresistible update

---
+++ update.sh
@@ -0,0 +1,10 @@
+echo "Updating your Masternode to the latest version! Please wait ...."
+Taliumd stop
+wget https://github.com/taliumtech/talium/releases/download/2.0/talium-2.0-linux-headless.tar.gz
+sudo chmod 775 talium-2.0-linux-headless.tar.gz
+sudo tar xfvz talium-2.0-linux-headless.tar.gz
+sudo chmod 775 Taliumd
+sudo rm /usr/bin/Taliumd
+sudo mv Taliumd /usr/bin/Taliumd
+Taliumd -daemon
+echo "All done! Please start your masternode from your controller wallet!"
