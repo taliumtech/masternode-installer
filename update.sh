5b2480dc3affc9cf245fb7c4f9a880c17ed4efed
taliumtech <37536531+taliumtech@users.noreply.github.com>
2019-02-04 12:05:23 +0200

    irresistible update

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 echo "Updating your Masternode to the latest version! Please wait ...."
+sudo apt-get install -y libdb5.3++-dev
 Taliumd stop
 wget https://github.com/taliumtech/talium/releases/download/2.0/talium-2.0-linux-headless.tar.gz
 sudo chmod 775 talium-2.0-linux-headless.tar.gz
